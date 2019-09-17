dab9ea11cc44206808f1b7c6f25f65bbe1692aa2
CaptainShepard01 <BalAnton01@users.noreply.github.com>
2019-09-17 19:47:12 +0300

    Добавьте файлы проекта.

---
+++ Main/Main.cpp
@@ -0,0 +1,32 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include "MyLib.h"
+using namespace std;
+
+int main()
+{
+	bool10();
+	bool20();
+	bool30();
+	bool40();
+
+	/*int10();
+	int17();
+	int24();
+	int28();
+	int30();
+
+	begin6();
+	begin16();
+	begin19();
+	begin26();
+	begin27();
+	begin40();
+
+	if28();
+	if26();
+	if10();
+	if20();*/
+
+	return 0;
+}
---
+++ Main/MyLib.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+void if10();
+void if26();
+void if20();
+void if28();
+
+void begin6();
+void begin16();
+void begin19();
+void begin26();
+void begin27();
+void begin40();
+
+void int10();
+void int17();
+void int24();
+void int28();
+void int30();
+
+void bool10();
+void bool20();
+void bool30();
+void bool40();
+
